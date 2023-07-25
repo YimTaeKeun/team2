891acb55d1c7cc8c011261aa6ec21d226b227eaf
YimTaekeun <ytk030305@gmail.com>
2023-07-25 16:02:04 +0900

    modified logo
    logo 최종 수

--- shopping_mall/src/Sale/Sale_nav.js
+++ shopping_mall/src/Sale/Sale_nav.js
@@ -1,6 +1,6 @@
 const Sale_nav = () => {
     return <div className="sale_nav">
-        <h1><strong>태연희 세일</strong></h1>
+        <h1><strong><a href='../App.js'>태연희</a><a href='./Sale_nav'> 세일</a></strong></h1>
     </div>
 }
 export default Sale_nav;
