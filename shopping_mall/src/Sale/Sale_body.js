48dd9c97ca28ca1af3a13154a0afa43d313b66e1
YimTaekeun <ytk030305@gmail.com>
2023-07-25 21:22:15 +0900

    Update Sale_body.js

--- shopping_mall/src/Sale/Sale_body.js
+++ shopping_mall/src/Sale/Sale_body.js
@@ -1,6 +1,9 @@
+import Product_list from "./Product_list";
+import Ad from "./Ad";
 const Sale_body = () => {
     return <div>
-        
+        <Product_list />
+        <Ad />
     </div>
 }
 export default Sale_body;
