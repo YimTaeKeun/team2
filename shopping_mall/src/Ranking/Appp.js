fd1eebebe01b156dc4c52015746a6d45e50972ba
jeonheejae1012 <orangecomet007@naver.com>
2023-07-26 02:11:35 +0900

    heejae
    11

---
+++ shopping_mall/src/Ranking/Appp.js
@@ -0,0 +1,11 @@
+import Sale_nav from "./Sale_nav";
+import Sale_body from "./Sale_body";
+import Ad from "./Ad";
+const Sale = () => {
+    return <div>
+        <Sale_nav />
+        <Sale_body />
+        <Ad />
+    </div>
+}
+export default Sale;
