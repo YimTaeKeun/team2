795a93c54b49fc7c65ecab2b91cdddc04f95985e
YimTaekeun <ytk030305@gmail.com>
2023-07-26 16:18:41 +0900

    Ranking 화면만 보이도록 함
    Ranking 화면만 보이도록 함

--- shopping_mall/src/App.js
+++ shopping_mall/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Sale from './Sale/Sale';
+import Appp from './Ranking/Appp';
 
 function App() {
   return (
     <div className="App">
-      {/* 아래 코드는 Sale 화면을 확인하기 위해 작성함 */}
-      <Sale />
+      {/* 아래 코드는 Ranking 화면을 확인하기 위해 작성함 */}
+      <Appp />
     </div>
   );
 }
