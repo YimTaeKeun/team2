da5874b94d1075d58b537247d5a10cd134f0d450
jeonheejae1012 <orangecomet007@naver.com>
2023-07-26 18:46:42 +0900

    .
    .

--- shopping_mall/src/Ranking/Appp.js
+++ shopping_mall/src/Ranking/Appp.js
@@ -1,6 +1,6 @@
 import Rank_nav from "./Rank_nav";
 import Rank_body from "./Rank_body";
-import advertise from "./advertise";
+import Advertise from "./Advertise";
 const Rank = () => {
     return <div>
         <Rank_nav />
