7ebb871814c8c0984be8960a485ad43045f1fcc2
YimTaekeun <ytk030305@gmail.com>
2023-07-30 02:37:38 +0900

    Add Home Screen
    Add Home Screen

--- shopping_mall/src/App.js
+++ shopping_mall/src/App.js
@@ -3,15 +3,20 @@ import Sale from './Sale/Sale';
 import Appp from './Ranking/Appp';
 import Man from './Gender_man/Man';
 import Woman from './Gender_woman/Woman';
+import Main_nav from './Main_nav';
 
 function App() {
   return (
     <div className="App">
+      {/* 이 부분 부터 메인 화면을 꾸미기 위한 코드 입니다. */}
+      <Main_nav />
+      {/* --------------- */}
+
       {/* 아래 코드는 Ranking 화면을 확인하기 위해 작성함 */}
       {/* <Appp/> */}
       {/* <Sale /> */}
       {/* <Man/> */}
-      <Woman/>
+      {/* <Woman/> */}
     </div>
   );
 }
---
+++ shopping_mall/src/Main_nav.js
@@ -0,0 +1,12 @@
+import search_img from './Sale/img/search.png';
+const Main_nav = () => {
+    return <div className="sale_nav">
+        <div></div>
+        <h1><strong><a href="./App.js">태연희 쇼핑몰</a></strong></h1>
+        <div>
+            <input type="text" placeholder="검색"/>
+            <img src={search_img}/>
+        </div>
+    </div>
+}
+export default Main_nav;
