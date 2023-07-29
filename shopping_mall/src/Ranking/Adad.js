ab0a16cf7e4ef98febbd15beb5ed6c2a13c6a42e
jeonheejae1012 <orangecomet007@naver.com>
2023-07-29 18:19:23 +0900

    ㅋㅋ
    ㅋㅋ

--- shopping_mall/src/Ranking/Adad.js
+++ shopping_mall/src/Ranking/Adad.js
@@ -1,10 +1,10 @@
-import one from './advimg/Advertisementment.png';
+import onead from './advimg/Advertisementment.png';
 const Ad = () => {
-    let img_arr = [one];
+    let img_arr = [onead];
     let a = Math.floor(Math.random() * 5);
     let path = './advimg/' + a + '.png';
     return <div className='Adad'>
-        {/* <img src={img_arr[a]}/> */}
+        <img src={img_arr[a]}/>
     </div>
 }
 export default Ad;
