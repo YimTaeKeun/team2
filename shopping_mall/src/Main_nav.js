84c7dcfab4d0da243966b572f3cb5ded64306d4d
YimTaekeun <ytk030305@gmail.com>
2023-07-30 14:24:27 +0900

    로고가 가운데 오도록 수정
    로고가 가운데 오도록 수정

--- shopping_mall/src/Main_nav.js
+++ shopping_mall/src/Main_nav.js
@@ -2,7 +2,7 @@ import search_img from './Sale/img/search.png';
 const Main_nav = () => {
     return <div className="sale_nav">
         <div></div>
-        <h1><strong><a href="./App.js">태연희 쇼핑몰</a></strong></h1>
+        <h1 className='logo_name'><strong><a href="./App.js">태연희 쇼핑몰</a></strong></h1>
         <div>
             <input type="text" placeholder="검색"/>
             <img src={search_img}/>
