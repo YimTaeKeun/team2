63f4c863a60dbccac2b40c596f959fedb743c53b
YimTaekeun <ytk030305@gmail.com>
2023-07-31 21:14:48 +0900

    검색 기능 활성화
    검색 시 무신사 페이지로 넘어가도록 함

--- shopping_mall/src/Sale/Sale_nav.js
+++ shopping_mall/src/Sale/Sale_nav.js
@@ -1,24 +1,17 @@
 import search_img from './img/search.png';
-import Product_inform from '../Product_inform/Product_inform';
 import React from 'react';
-import { Route, Router } from 'react-router-dom';
-// import {useNavigate} from 'react-router-dom';
+import { useState } from 'react';
 const Sale_nav = () => {
-    const searchProduct = () => {
-        console.log("hello");
-        <Route path='./Product_inform/Product_inform' Component={Product_inform} />
+    const [state, setState] = useState();
+    function changeText(){
+        setState('https://www.musinsa.com/search/musinsa/integration?q=' + document.getElementById('search_input').value);
     }
-    // const navigate = useNavigate();
-    // const searchProduct = () => {
-    //     navigate("https://naver.com");
-    //     document.querySelector('.search_input').value = "";
-    // }
     return <div className="sale_nav">
         <div></div>
         <h1 className='logo_name'><strong><a href='../App.js'>태연희</a><a href='./Sale_nav'> 세일</a></strong></h1>
         <div>
-            <input type="text" placeholder="검색" class="search_input"/>
-            <img src={search_img} onClick={searchProduct}/>
+            <input id='search_input' type="text" placeholder="검색" class="search_input" onChange={changeText}/>
+            <a href={state}><img src={search_img}/></a>
         </div>
     </div>
 }
