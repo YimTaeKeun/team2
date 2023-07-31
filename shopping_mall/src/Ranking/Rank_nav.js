44aca4a9db6c9ea6de3b9550d78e355f381d3172
YimTaekeun <ytk030305@gmail.com>
2023-07-31 18:31:09 +0900

    Modified Logo Font Style

--- shopping_mall/src/Ranking/Rank_nav.js
+++ shopping_mall/src/Ranking/Rank_nav.js
@@ -2,7 +2,7 @@ import search_img from './rankimage/search.png';
 const Rank_nav = () => {
     return <div className="sale_nav">
         <div></div>
-        <h1><strong><a href='../App.js'>TYH</a><a href='./Rank_nav'> 랭킹</a></strong></h1>
+        <h1 className='logo_name'><strong><a href='../App.js'>TYH</a><a href='./Rank_nav'> 랭킹</a></strong></h1>
         <div>
             <input type="text" placeholder="검색"/>
             <img src={search_img}/>
