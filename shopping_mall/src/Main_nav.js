5ab2860e0ff9b374cee63ffb971c18ed199cdd07
YimTaekeun <ytk030305@gmail.com>
2023-08-02 19:22:48 +0900

    main_nav바 검색 구현

--- shopping_mall/src/Main_nav.js
+++ shopping_mall/src/Main_nav.js
@@ -1,11 +1,16 @@
 import search_img from './Sale/img/search.png';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 const Main_nav = () => {
+    const [state, setState] = useState();
+    const onChangeInput = () => {
+        setState("https://www.musinsa.com/search/musinsa/integration?q=" + document.getElementsByClassName('main_search_input').value);
+    }
     return <div className="sale_nav">
         <h1 className='main_logo_name'><strong><a href="./App.js">TYH Shopping Mall</a></strong></h1>
         <div>
-            <input type="text" placeholder="검색" className='main_search_input'/>
-            <img src={search_img}/>
+            <input type="text" placeholder="검색" className='main_search_input' onChange={onChangeInput}/>
+            <a href={state}><img src={search_img}/></a>
         </div>
         <div>
             {/* 이 곳에 세일, 성별, 사계절, 랭킹을 택하는 팝업화면 코드를 넣을 예정 */}
