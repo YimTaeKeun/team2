79eb738c95d24bae8f7f0c18c77fb07697997224
YimTaekeun <ytk030305@gmail.com>
2023-08-03 00:38:15 +0900

    팝업화면 만들다가 중도포기

---
+++ shopping_mall/src/Main_nav_DownPopup.js
@@ -0,0 +1,6 @@
+const Main_nav_DownPopup = ({down_popup}) => {
+    return <div className="DownPopup">
+        {down_popup}
+    </div>
+}
+export default Main_nav_DownPopup;
--- shopping_mall/src/Main_nav_popup.js
+++ shopping_mall/src/Main_nav_popup.js
@@ -1,6 +1,9 @@
+import Main_nav_DownPopup from "./Main_nav_DownPopup";
 const Main_nav_popup = ({list, down_popup}) => {
     return <div className="popup">
         {list}
+        {/* 중도 포기 */}
+        {/* {down_popup[list].map((each) => (<Main_nav_DownPopup down_popup={each} />))} */}
     </div>
 }
 export default Main_nav_popup;
