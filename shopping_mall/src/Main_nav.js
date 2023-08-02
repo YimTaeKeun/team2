1e61c82197ae891f5c650fd9633b3042ba41219c
YimTaekeun <ytk030305@gmail.com>
2023-08-02 19:56:56 +0900

    Updated down_popup

--- shopping_mall/src/Main_nav.js
+++ shopping_mall/src/Main_nav.js
@@ -3,6 +3,7 @@ import Main_nav_popup from './Main_nav_popup';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 const Main_nav = () => {
+    const down_popup = {"세일": ["폭탄 세일", "한정 세일"], "성별": ["남", "여"], "사계절": ["봄", "여름", "가을", "겨울"], "랭킹": ["금주", "한달", "전체"]};
     const popup_kind = ["세일", "성별", "사계절", "랭킹"];
     const [state, setState] = useState();
     const onChangeInput = () => {
@@ -16,7 +17,7 @@ const Main_nav = () => {
         </div>
         {/* 이 곳에 세일, 성별, 사계절, 랭킹을 택하는 팝업화면 코드를 넣을 예정 */}
         <div className='nav_popup'>
-        {popup_kind.map((each) => (<Main_nav_popup list={each}/>))}
+        {popup_kind.map((each) => (<Main_nav_popup list={each} down_popup={down_popup}/>))}
         </div>
         
     </div>
--- shopping_mall/src/Main_nav_popup.js
+++ shopping_mall/src/Main_nav_popup.js
@@ -1,4 +1,4 @@
-const Main_nav_popup = ({list}) => {
+const Main_nav_popup = ({list, down_popup}) => {
     return <div className="popup">
         {list}
     </div>
