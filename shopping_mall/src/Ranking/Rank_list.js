75262321dea84f3cf975c107825fb3218c05e93e
jeonheejae1012 <orangecomet007@naver.com>
2023-08-03 15:27:28 +0900

    랭킹 몇번째 인지 만들기 성공

--- shopping_mall/src/Ranking/Rank_list.js
+++ shopping_mall/src/Ranking/Rank_list.js
@@ -1,4 +1,5 @@
 import hoodyhoody1 from '../Product_img/Top/Yaledog_boy.png';
+import hoody_2 from '../Product_img/Top/Yaledog_boy.png'
 import blackgardigan from '../Product_img/Top/Black_gardigan_boy.png';
 import bluebluegirl from '../Product_img/Top/Blueshirt_girl.png';
 import pants_2 from '../Product_img/Pants/Jogger_Pants_girl.jpg';
@@ -8,9 +9,14 @@ import pants_3 from '../Product_img/Pants/Jogger_Pants_girl.jpg';
 import Rank_product from './Rank_product';
 
 const Rank_list = () => {
-    const heejae = [hoodyhoody1, pants_3, bluebluegirl,blackgardigan,pants_2,handsomeboy,adidasgirlboy];
+    const heejae = [hoodyhoody1,hoody_2, pants_3, bluebluegirl,blackgardigan,pants_2,handsomeboy,adidasgirlboy];
     let Rank_list = []
-    for(let i = 0; i < heejae.length; i++) Rank_list[i] = {CCLLOOTTHH: heejae[i], like: Math.floor(Math.random() * 100)};
+    for(let i = 0; i < heejae.length; i++) Rank_list[i] = {CCLLOOTTHH: heejae[i], Sale: Math.floor(Math.random() * 10) + 5, Name:heejae[i]};
+    Rank_list.sort(function(a, b){
+        if (a.Rank < b.Rank) return 1;
+        else if(a.Rank === b.Rank) return 0;
+        else return -1;
+    });
     return <div className='Rank_list'>
         <div>
         <h4>인기가 높은 순을 기준으로 정렬했습니다.</h4>
