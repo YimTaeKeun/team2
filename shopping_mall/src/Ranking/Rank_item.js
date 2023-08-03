2cf43a9bd550c6cf05a0cb4092fda79d3450a8ee
jeonheejae1012 <orangecomet007@naver.com>
2023-08-03 15:35:15 +0900

    아니돼오

---
+++ shopping_mall/src/Ranking/Rank_item.js
@@ -0,0 +1,15 @@
+// import { useNavigate } from "react-router-dom";
+// const Rank_item = ({list}) => {
+//     const movePage = useNavigate();
+//     const Cloth_inform = list.Cloth;
+//     const sale = list.Sale;
+//     const Cloth_name = list.Name;
+//     function goInform(){
+//         movePage('/Product_inform/Product_inform?Cloth=' + Cloth_inform + "&Sale=" + sale + "&Name=" + Cloth_name);
+//     }
+//     return <div className='Rank_item'>
+//         <img src={list.CCLLOOTTHH} onClick={goInform}/>
+//         <h2>{list.Rank}% 번 째 인기!</h2>
+//     </div>
+// }
+// export default Rank_item;
