39cfadbbab66f3b23530a066932cbad07056a150
jeonheejae1012 <orangecomet007@naver.com>
2023-08-09 17:42:14 +0900

    숫자 나오게 하기
    안됌

--- shopping_mall/src/Ranking/Rank_item.js
+++ shopping_mall/src/Ranking/Rank_item.js
@@ -1,15 +1,15 @@
-// import { useNavigate } from "react-router-dom";
-// const Rank_item = ({list}) => {
-//     const movePage = useNavigate();
-//     const Cloth_inform = list.Cloth;
-//     const sale = list.Sale;
-//     const Cloth_name = list.Name;
-//     function goInform(){
-//         movePage('/Product_inform/Product_inform?Cloth=' + Cloth_inform + "&Sale=" + sale + "&Name=" + Cloth_name);
-//     }
-//     return <div className='Rank_item'>
-//         <img src={list.CCLLOOTTHH} onClick={goInform}/>
-//         <h2>{list.Rank}% 번 째 인기!</h2>
-//     </div>
-// }
-// export default Rank_item;
+import { useNavigate } from "react-router-dom";
+const Rank_item = ({list}) => {
+    const movePage = useNavigate();
+    const CCLLOOTTHH_inform= list.CCLLOOTTHH;
+    const sale = list.Rank;
+    const CCLLOOTTHH_name = list.CCLLOOTTHH;
+    function goInform(){
+        movePage('/Product_inform/Product_inform?Cloth=' + CCLLOOTTHH_inform + "&Sale=" + sale + "&Name=" + CCLLOOTTHH_name);
+    }
+    return <div className='Rank_item'>
+        <img src={list.CCLLOOTTHH} onClick={goInform}/>
+        <h2>{list.Rank}% 번 째 인기!</h2>
+    </div>
+}
+export default Rank_item;
