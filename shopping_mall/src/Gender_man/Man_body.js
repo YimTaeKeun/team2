5d428bad233093783fd097e98dd4edfc342364a0
jooya38 <hyj0102809@gmail.com>
2023-08-14 23:33:32 +0900

    TopBottom버튼 추가
    CSS까지 설정 후 모든 body에 추가

--- shopping_mall/src/Gender_man/Man_body.js
+++ shopping_mall/src/Gender_man/Man_body.js
@@ -1,12 +1,14 @@
 import Man_list from "./Man_list";
+import TopBottom_Btn from "../TopBottom_Btn";
 
 const Man_body = ()=> {
     return <div>
-        <div class="TopPants-Btn">
-            <a href="#Man_Top"><input class="TPBtn" type = "button" value="상의"/></a>
-            <a href="#Man_Bottom"><input class="TPBtn" type = "button" value="하의"/></a>
+        <div className="TopPants-Btn">
+            <a href="#Man_Top"><input className="TPBtn" type = "button" value="상의"/></a>
+            <a href="#Man_Bottom"><input className="TPBtn" type = "button" value="하의"/></a>
         </div>
         <Man_list/>
+        <TopBottom_Btn/>
     </div>
 }
 
--- shopping_mall/src/Gender_woman/Woman_body.js
+++ shopping_mall/src/Gender_woman/Woman_body.js
@@ -3,9 +3,9 @@ import Woman_list from "./Woman_list";
 
 const Woman_body = () => {
     return <div>
-        <div class="TopPants-Btn">
-            <a href="#Woman_Top"><input class="TPBtn" type = "button" value="상의"/></a>
-            <a href="#Woman_Bottom"><input class="TPBtn" type = "button" value="하의"/></a>
+        <div className="TopPants-Btn">
+            <a href="#Woman_Top"><input className="TPBtn" type = "button" value="상의"/></a>
+            <a href="#Woman_Bottom"><input className="TPBtn" type = "button" value="하의"/></a>
         </div>
         <Woman_list/>
         <TopBottom_Btn/>
