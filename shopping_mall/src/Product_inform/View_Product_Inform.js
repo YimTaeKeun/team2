8ec1f8e29d4e9cc5461cb662d298849b7cfbe320
YimTaekeun <ytk030305@gmail.com>
2023-08-22 01:27:12 +0900

    상품 명과 사이즈 정도 나오는 것 까지 완료

--- shopping_mall/src/Product_inform/View_Product_Inform.js
+++ shopping_mall/src/Product_inform/View_Product_Inform.js
@@ -1,6 +1,16 @@
 const View_Product_Inform = ({Cloth_name, Sale_imform}) => {
-    return <div>
-        Hello
+    return <div className="View_Product_Inform">
+        <div className="cloth_name">
+            {Cloth_name}
+        </div>
+        <div className="Size">
+            <div>XS</div>
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXS</div>
+        </div>
     </div>
 }
 export default View_Product_Inform;
