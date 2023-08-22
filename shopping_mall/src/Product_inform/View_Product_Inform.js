95f228bb127dcc1a512a3a502484ff76c284e10d
YimTaekeun <ytk030305@gmail.com>
2023-08-22 23:21:56 +0900

    좋아요 구현 완료. 할인가 즉각 랜덤 안되도록 수정 해야함.

--- shopping_mall/src/Product_inform/View_Product_Inform.js
+++ shopping_mall/src/Product_inform/View_Product_Inform.js
@@ -1,4 +1,10 @@
+import heartIMG from '../EtcSrc/heart.png';
+import { useState } from 'react';
 const View_Product_Inform = ({Cloth_name, Sale_imform}) => {
+    const [state, setState] = useState(Math.floor(Math.random() * 1000) + 1);
+    const upHeart = () => {
+        setState(parseInt(document.getElementById('count').innerText) + 1);
+    }
     return <div className="View_Product_Inform">
         <div className="cloth_name">
             상품명: {Cloth_name}
@@ -16,6 +22,10 @@ const View_Product_Inform = ({Cloth_name, Sale_imform}) => {
         </div>
         <div className="SalePercent">
             할인율: {Sale_imform}%
+        </div>
+        <div className="like_btn">
+            <img src={heartIMG} onClick={upHeart}/>
+            <div> 좋아요 <div className='count' id='count'>{state}</div>개</div>
         </div>
     </div>
 }
--- shopping_mall/src/Product_inform/nav.js
+++ shopping_mall/src/Product_inform/nav.js
@@ -2,7 +2,7 @@ import search_img from '../Sale/img/search.png';
 const nav = () => {
     return <div className="sale_nav">
         <div></div>
-        <h1 className='logo_name'><strong><a href='./Product_inform.js'> 상품 정보</a></strong></h1>
+        <h1 className='logo_name'><strong><a href='/'> 상품 정보</a></strong></h1>
         <div>
             <input type="text" placeholder="검색"/>
             <img src={search_img}/>
