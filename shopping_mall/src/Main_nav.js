d014d5df612f694993e859dabded82f8a3fa4fec
YimTaekeun <ytk030305@gmail.com>
2023-08-23 14:26:49 +0900

    메인 검색 수정

--- shopping_mall/src/Main_nav.js
+++ shopping_mall/src/Main_nav.js
@@ -4,12 +4,12 @@ import { useState } from 'react';
 const Main_nav = () => {
     const [state, setState] = useState();
     const onChangeInput = () => {
-        setState("https://www.musinsa.com/search/musinsa/integration?q=" + document.getElementsByClassName('main_search_input').value);
+        setState("https://www.musinsa.com/search/musinsa/integration?q=" + document.getElementById('main_search_input').value);
     }
     return <div className="sale_nav">
         <h1 className='main_logo_name'><strong><a href="./App.js">TYH Shopping Mall</a></strong></h1>
         <div>
-            <input type="text" placeholder="검색" className='main_search_input' onChange={onChangeInput}/>
+            <input type="text" placeholder="검색" className='main_search_input' onChange={onChangeInput} id='main_search_input'/>
             <a href={state}><img src={search_img}/></a>
         </div>
         {/* 이 곳에 세일, 성별, 사계절, 랭킹을 택하는 팝업화면 코드를 넣을 예정 */}
