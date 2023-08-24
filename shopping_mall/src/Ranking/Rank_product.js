2131d99c0819f3c417c0fd2b6bedb988d61daecd
jeonheejae1012 <orangecomet007@naver.com>
2023-08-24 17:57:31 +0900

    사진 수정과 top 가운데 정렬
    ㅋ

--- shopping_mall/src/Ranking/Rank_product.js
+++ shopping_mall/src/Ranking/Rank_product.js
@@ -1,7 +1,19 @@
-const Product_item = ({list}) => {
-    return <div className='Product_item'>
-        <img src={list.CCLLOOTTHH}/>
-        <h2> {list.Rank} 번 째 인기! </h2>
-    </div>
-}
-export default Product_item;
+const Product_item = ({ list }) => {
+    const imageStyle = {
+      border: '2px solid #ccc', // 테두리 스타일 및 두께를 설정합니다.
+    };
+  
+    const textStyle = {
+      textAlign: 'center',
+    };
+  
+    return (
+      <div className='Product_item'>
+        <img src={list.CCLLOOTTHH} style={imageStyle} alt={`Product ${list.Rank}`} />
+        <h2 style={textStyle}>Top {list.Rank}</h2>
+      </div>
+    );
+  }
+  
+  export default Product_item;
+  
