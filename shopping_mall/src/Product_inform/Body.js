a7db27d8b3fd992354993f68e718eae8d4b5b3aa
YimTaekeun <ytk030305@gmail.com>
2023-08-25 13:31:59 +0900

    상품 정보에 위아래 이동 버튼 추가

--- shopping_mall/src/Product_inform/Body.js
+++ shopping_mall/src/Product_inform/Body.js
@@ -2,6 +2,7 @@ import { useLocation, useSearchParams } from "react-router-dom";
 import View_Product_Inform from "./View_Product_Inform";
 import View_Product_Img from "./View_Product_Img";
 import View_Review from "./View_Review";
+import TopBottom_Btn from "../TopBottom_Btn";
 const Body = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const Image_inform = searchParams.get("Cloth");
@@ -12,6 +13,7 @@ const Body = () => {
         <View_Product_Img img_src={Image_inform} />
         <View_Product_Inform Cloth_name={Cloth_name} Sale_imform={Sale_inform} Cloth_Price={Cloth_Price}/>
         <View_Review />
+        <TopBottom_Btn />
         {/* 아래 코드는 디버깅 용도로 남겨둔 것임 */}
         {/* <h1>{Cloth_name}</h1> */}
     </div>
