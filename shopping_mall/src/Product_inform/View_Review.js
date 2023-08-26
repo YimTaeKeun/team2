20cb9ca2d0723831c5735f1723eb4b575e5710d5
YimTaekeun <ytk030305@gmail.com>
2023-08-26 15:09:34 +0900

    리뷰 작성 기능만 추가 완료

--- shopping_mall/src/Product_inform/View_Review.js
+++ shopping_mall/src/Product_inform/View_Review.js
@@ -1,12 +1,24 @@
 import { useState } from "react";
+import View_Review_Items from "./View_Review_Items";
 const View_Review = () => {
     const [reviewBox, setReviewBox] = useState("");
+    const [reviews, setReviews] = useState([]);
     const onChangeBox = () => {
         setReviewBox(document.getElementById('review_box').value);
     }
+    const clickBtn = () => {
+        setReviews([...reviews, reviewBox]);
+        setReviewBox("");
+        console.log(reviews);
+    }
     return <div className="view_review">
-        <input type="text" placeholder="리뷰를 입력해주세요." style={{width:'1080px', height:'30px'}} value={reviewBox} id="review_box" onChange={onChangeBox}/>
-        <button>입력하기</button>
+        <div>
+            <input type="text" placeholder="리뷰를 입력해주세요." style={{width:'1080px', height:'30px'}} value={reviewBox} id="review_box" onChange={onChangeBox}/>
+            <button onClick={clickBtn}>입력하기</button>
+        </div>
+        <div className="review_list">
+            {reviews.map((each) => (<View_Review_Items txt={each}/>))}
+        </div>
     </div>
 }
 export default View_Review;
---
+++ shopping_mall/src/Product_inform/View_Review_Items.js
@@ -0,0 +1,6 @@
+const View_Review_Items = ({txt}) => {
+    return <div className="View_Review_Items">
+        {txt}
+    </div>
+}
+export default View_Review_Items;
